3a5a6ea3efa6aae9bb64e31c9501770e4d3c5566
Brady <47389606+bradybellini@users.noreply.github.com>
2024-08-22 22:10:37 -0700

    creating a window

--- main.go
+++ main.go
@@ -1,12 +1,110 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	sdl "github.com/veandco/go-sdl2/sdl"
 )
 
-func main() {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic("Failed to initialize window")
+const (
+	windowWidth  = 800
+	windowHeight = 600
+	windowTitle  = "Go sdl Window"
+)
+
+type game struct {
+	window   *sdl.Window
+	renderer *sdl.Renderer
+}
+
+func initializeSDL() error {
+	var err error
+	var sdlFlags uint32 = sdl.INIT_EVERYTHING
+
+	if err := sdl.Init(sdlFlags); err != nil {
+		return fmt.Errorf("error initializing SDL2: %v", err)
 	}
-	
+
+	return err
+}
+
+func closeSDL() {
+
+}
+
+func newGame() *game {
+	g := &game{}
+
+	return g
+}
+
+func (g *game) init() error {
+	var err error
+
+	if g.window, err = sdl.CreateWindow(windowTitle, sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED, windowWidth, windowHeight, sdl.WINDOW_SHOWN); err != nil {
+		return fmt.Errorf("error creating window: %v", err)
+	}
+
+	if g.renderer, err = sdl.CreateRenderer(g.window, -1, sdl.RENDERER_ACCELERATED); err != nil {
+		return fmt.Errorf("error creating renderer: %v", err)
+	}
+	return err
+}
+
+func (g *game) close() {
+	if g != nil {
+		g.renderer.Destroy()
+		g.renderer = nil
+		g.window.Destroy()
+		g.window = nil
+	}
+}
+
+func (g *game) run() {
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch e := event.(type) {
+			case *sdl.QuitEvent:
+				return
+			case *sdl.KeyboardEvent:
+				if e.Type == sdl.KEYDOWN {
+					switch e.Keysym.Scancode {
+					case sdl.SCANCODE_ESCAPE:
+						return
+					}
+				}
+			}
+		}
+		g.renderer.Clear()
+
+		g.renderer.Present()
+
+		sdl.Delay(16)
+
+	}
+
+}
+
+func main() {
+	var err error
+
+	defer closeSDL()
+
+	if err = initializeSDL(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return
+	}
+
+	g := newGame()
+
+	defer g.close()
+
+	if err = g.init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return
+	}
+
+	g.run()
+
 }
